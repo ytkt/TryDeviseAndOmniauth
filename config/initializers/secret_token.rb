5baf1ee91083858200e1838489eb272dadfa832f
Yuta Kato <yyttkktt@gmail.com>
2013-12-16 22:31:37 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TryDeviseAndOmniauth::Application.config.secret_key_base = '602a5f67caa27e6770983ce32fce64e3c77485646a9c04229ca9bf901166eb83bf09f9b701e594531032563179d0b6596235f7104c1aacfd3d3cb6cd5cd42432'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TryDeviseAndOmniauth::Application.config.session_store :cookie_store, key: '_TryDeviseAndOmniauth_session'
