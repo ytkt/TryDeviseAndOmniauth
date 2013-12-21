f8c59c1659ff6b9f90d06a73f6b1cec5abe98aeb
Yuta Kato <yyttkktt@gmail.com>
2013-12-21 18:43:45 +0900

    facebookからのコールバックを処理してログインする

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  def self.find_for_facebook_oauth(auth)
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+
+    unless user
+      user = User.create( name:     auth.extra.raw_info.name,
+                          provider: auth.provider,
+                          uid:      auth.uid,
+                          email:    auth.info.email,
+                          token:    auth.credentials.token,
+                          password: Devise.friendly_token[0,20] )
+    end
+
+    return user
+  end
 end
