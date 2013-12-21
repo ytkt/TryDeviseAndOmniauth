8bba8ac8546e3ea90b9ce74aae9ca00e33181eba
Yuta Kato <yyttkktt@gmail.com>
2013-12-21 20:25:12 +0900

    ログイン後のパスを /home に変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ TryDeviseAndOmniauth::Application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
-  get "home/index"
+  get "home", to: "home#index", as: "user_root"
   root "welcome#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
