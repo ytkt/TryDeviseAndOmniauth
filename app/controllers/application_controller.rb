f0a7b2bcaf60884eff14aaa3b2368fbcd94e297f
Yuta Kato <yyttkktt@gmail.com>
2013-12-21 21:22:08 +0900

    未ログイン時のリダイレクト先をfacebook認証のページに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def authenticate_user!
+    session[:user_return_to] = env['PATH_INFO']
+    redirect_to user_omniauth_authorize_path(:facebook) unless user_signed_in?
+  end
 end
